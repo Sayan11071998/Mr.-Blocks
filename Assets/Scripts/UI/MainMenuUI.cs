30996ab2273e813466aa23e58ce6f2873e8e124d
Sayan Nandi <sayannandi11071998@gmail.com>
2025-01-23 23:24:02 +0530

    Working with game UI

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
+    public Button playButton;
+    public Button quitButton;
+
     private const int firstLevel = 1;
+
+    private void Awake() => AddListerers();
+
+    private void AddListerers()
+    {
+        playButton.onClick.AddListener(Play);
+        quitButton.onClick.AddListener(Quit);
+    }
 
     public void Play() => SceneManager.LoadScene(firstLevel);
 
     public void Quit()
     {
-        Application.Quit();
-        Debug.Log("Game Quit");
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
     }
 }
